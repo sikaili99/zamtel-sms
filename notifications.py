08eb217bfb955c848ee4bd6b8223bb29bc696261
Mathews Musukuma <33897492+Mathewsmusukuma@users.noreply.github.com>
2021-06-01 01:29:59 +0200

    Change from func to class (#2)

--- notifications.py
+++ notifications.py
@@ -1,5 +1,11 @@
-from sms import send_sms
+from sms import ZamtelSMSApi
+
+#instatiate the sms class
+zamtel_sms = ZamtelSMSApi()
 
 def send_notification(phone_number, message):
-    response = send_sms(phone_number,message)
+    response = zamtel_sms.send_sms(phone_number,message)
     return response
+
+
+send_notification('0966468776','Hello')
